5e1e293d2c030e96fa02a0b63cc6b621fea91aaa
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-08-07 15:13:30 +0530

    Add user login functionality and error handling

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -86,7 +86,46 @@ const loginUser = asyncHandler(async (req,res) => {
 
     const {email, username, password} = req.body
 
-    // TODO: validate the request body
+    if (!(username || email)) {
+        throw new ApiError(400,"usernamw or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "user logged In Successfully..."
+        )
+    )
 
 })
 
