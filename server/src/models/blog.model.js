8331011f710340d96cd21c7563425696095a9e5f
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-08-30 15:43:25 +0530

    Update blog controller to include category in createBlog endpoint

--- server/src/models/blog.model.js
+++ server/src/models/blog.model.js
@@ -8,6 +8,10 @@ const blogSchema = new mongoose.Schema({
   },
   thumbnail:{
     type: String,
+    // required: true,
+  },
+  category:{
+    type: String,
     required: true,
   },
   content:{
